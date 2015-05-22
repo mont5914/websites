08e36003fd35d8c49e07854b47b24b7b339c8e70
mont5914 <jmontgomery5914@gmail.com>
2015-05-22 09:14:28 -0700

    Intial commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+
+
+$(document)
+  .ready(function () {
+    $('.ryu').mouseenter(function () {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function () {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function () {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken')
+    .finish()
+    .show()
+    .animate(
+      {'left': '1020px'},
+      500,
+      function () {
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+    );
+  })
+  .mouseup(function () {
+    $('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+    // ryu goes back to his ready position
+  })
+  
+  $('.ryu').keydown(function () {
+
+ if (e.keyCode == 88) {
+       $('.ryu-cool').show();
+       $('.ryu-ready').hide(); 
+   }
+  })
+});
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
